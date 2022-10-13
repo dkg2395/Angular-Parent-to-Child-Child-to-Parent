51e846e28a863bbc835c261308501fd59060fd99
durgesh <dkg.2395@gmail.com>
2022-10-13 17:01:40 +0530

    pagination implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
 import { MovieTableComponent } from './movie-table/movie-table.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieDashboardComponent,
-    MovieTableComponent
+    MovieTableComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
